d5e3100bf176187c248e01ceef29717139df0683
Akito Ito <akito_ito@r.recruit.co.jp>
2018-09-06 14:35:49 +0900

    fix: chan

--- ch1/ex01.3/src/main.rs
+++ ch1/ex01.3/src/main.rs
@@ -1,8 +1,9 @@
-use std::sync::mpsc::channel;
+use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
+#[derive(PartialEq)]
 enum State {
     Thinking,
     Hungry,
@@ -11,26 +12,70 @@ enum State {
 
 struct Philosopher {
     id: u8,
+    left: Sender<i32>,
+    right: Sender<i32>,
+    rx: Arc<Mutex<Receiver<i32>>>,
 }
 
 impl Philosopher {
-    fn new(id: u8) -> Philosopher {
-        Philosopher { id: id }
+    fn new(
+        id: u8,
+        left: Sender<i32>,
+        right: Sender<i32>,
+        rx: Arc<Mutex<Receiver<i32>>>,
+    ) -> Philosopher {
+        Philosopher {
+            id: id,
+            left: left,
+            right: right,
+            rx: rx,
+        }
     }
 
-    fn eat(&self, mutex: &Mutex<Monitor>) {
+    fn pickup(&self, mutex: &Mutex<Monitor>) -> Result<(), ()> {
         match mutex.try_lock() {
-            Ok(ref mut mutex) => {
-                if mutex.states[self.id as usize] == State::Thinking {};
-                return;
+            Ok(ref mut monitor) => {
+                let right = ((self.id + 1) % 5) as usize;
+                let left = ((self.id + 4) % 5) as usize;
+                if monitor.states[self.id as usize] == State::Hungry
+                    && monitor.states[right] == State::Eating
+                    && monitor.states[left] == State::Eating
+                {
+                    monitor.states[self.id as usize] = State::Eating;
+                };
+                Ok(())
             }
-            _ => (),
-        };
+            _ => Err(()),
+        }
+    }
+
+    fn eat(&self) {
+        println!("{} is eating", self.id);
+        thread::sleep(Duration::from_millis(2000));
+    }
+
+    fn wait(&self) {
+        let r = self.rx.lock().unwrap();
+        r.recv();
+    }
+
+    fn putdown(&self, mutex: &Mutex<Monitor>) -> Result<(), ()> {
+        let mut lock = mutex.lock().unwrap();
+        lock.states[self.id as usize] = State::Thinking;
+        self.left.send(0).unwrap();
+        self.right.send(0).unwrap();
+        Ok(())
     }
 }
 
 struct Monitor {
     states: Vec<State>,
+}
+
+fn makeChan() -> (Sender<i32>, Arc<Mutex<Receiver<i32>>>) {
+    let (tx, rx): (Sender<i32>, Receiver<i32>) = channel();
+
+    (tx, Arc::new(Mutex::new(rx)))
 }
 
 fn main() {
@@ -44,15 +89,44 @@ fn main() {
         ],
     }));
 
+    let channels = vec![makeChan(), makeChan(), makeChan(), makeChan(), makeChan()];
+
     let philosophers = vec![
-        Philosopher::new(0),
-        Philosopher::new(1),
-        Philosopher::new(2),
-        Philosopher::new(3),
-        Philosopher::new(4),
+        Philosopher::new(
+            0,
+            channels[4].0.clone(),
+            channels[1].0.clone(),
+            channels[0].1.clone(),
+        ),
+        Philosopher::new(
+            1,
+            channels[0].0.clone(),
+            channels[2].0.clone(),
+            channels[1].1.clone(),
+        ),
+        Philosopher::new(
+            2,
+            channels[1].0.clone(),
+            channels[3].0.clone(),
+            channels[2].1.clone(),
+        ),
+        Philosopher::new(
+            3,
+            channels[2].0.clone(),
+            channels[4].0.clone(),
+            channels[3].1.clone(),
+        ),
+        Philosopher::new(
+            4,
+            channels[3].0.clone(),
+            channels[0].0.clone(),
+            channels[4].1.clone(),
+        ),
     ];
 
-    // let (tx, rx) = channel();
+    let (tx, rx): (Sender<i32>, Receiver<i32>) = channel();
+    let r_mut = Arc::new(Mutex::new(rx));
+
     let handles: Vec<_> = philosophers
         .into_iter()
         .map(|p| {
@@ -60,7 +134,15 @@ fn main() {
             thread::spawn(move || -> () {
                 println!("id {} is spawn", p.id);
                 loop {
-                    p.eat(&mutex);
+                    match p.pickup(&mutex) {
+                        Ok(_) => {
+                            p.eat();
+                            p.putdown(&mutex);
+                        }
+                        _ => {
+                            p.wait();
+                        }
+                    }
                 }
             })
         }).collect();
